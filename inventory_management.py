763f4a3fa6ea4d8cdf7d390acc86e5f3c62496ca
Davids Berzins <116670695+cfunkz@users.noreply.github.com>
2024-01-20 22:38:49 +0000

    Update inventory_management.py

--- inventory_management.py
+++ inventory_management.py
@@ -1,85 +1,111 @@
 import uuid
+import json
 
-items = {}
+
+def load_items_from_file():
+    try:
+        with open('inventory.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+
+items = load_items_from_file()
+
+
+def save_json(filename):
+    with open(filename, 'w') as file:
+        json.dump(items, file)
+
+
+def delete_item(input_value):
+    for name, details in items.items():
+        if input_value in [name, details["barcode"]]:
+            del items[name]
+            print(f"Item deleted successfully.")
+            break
+    else:
+        print("Item not found in inventory.")
 
 
 def add_item(name, barcode, quantity):
+    # IF item name not in items, or barcode not in item values
     if int(barcode) == 0:
         code = uuid.uuid4()
         barcode = str(code)[:5]
-    # IF item name not in items, or barcode not in item values
     if name not in items and barcode not in [item["barcode"] for item in items.values()]:
         items[name] = {
             "barcode": barcode,
             "quantity": int(quantity)
         }
-        print("Item added successfully.")
+        save_json('inventory.json')
+        print(f"Item added successfully [{name} | {barcode} | {quantity}]")
     else:
         print("Item exists. Try updating or removing.")
 
 
 def update_item(input_value, quantity):
-    for name, barcode in items.items():
-        if input_value in [name, barcode["barcode"]]:
+    for name, details in items.items():
+        if input_value in [name, details["barcode"]]:
             items[name]["quantity"] += int(quantity)
+            save_json('inventory.json')
             print(f"Quantity updated successfully. Current stock for {name}: {items[name]['quantity']}")
             break
-    else:
-        print("Item not found in inventory.")
-
-
-def remove_item(input_value, quantity):
-    for name, barcode in items.items():
-        if input_value in [name, barcode["barcode"]]:
-            items[name]["quantity"] -= int(quantity)
-            print(f"Quantity updated successfully. Current stock for {name}: {items[name]['quantity']}")
-            break
-    else:
-        print("Item not found in inventory.")
+        else:
+            print("Item not found in inventory.")
 
 
 def find_item(input_value):
-    for name, barcode in items.items():
-        if input_value in [name, barcode["barcode"]]:
-            print(f"Item found: {name} | {barcode}")
+    for name, details in items.items():
+        if input_value in [name, details["barcode"]]:
+            print(f"Item found: {name} | {details}")
         else:
             print(f"Item not found in inventory.")
 
 
+def view_items():
+    print("Name\t\t| Barcode\t| Quantity")
+    print("="*40)
+    for name, details in items.items():
+        barcode = details["barcode"]
+        quantity = details["quantity"]
+        print(f"{name}\t\t| {barcode}\t| {quantity}")
+    print("="*40)
+
+
 def main():
     while True:
+        print("Current Items: ", len(items))
         print("""
             ####################
             ## 1. Add Item    ##
             ## 2. Update      ##
-            ## 3. Remove Item ##
-            ## 4. Find Item   ##
+            ## 3. Find Item   ##
+            ## 4. View Items   ##
             ## 5. Exit        ##
             ####################
               """)
 
         choice = input("Type an option from above: ")
 
-        if choice not in ["1", "2", "3", "4", "5"]:
+        if choice not in ["1", "2", "3", "4"]:
             print("Error: Invalid choice.")
         else:
             choice = int(choice)
-            if choice == 1:
+            if choice == 1: # Add Item
                 name = input("Name: ")
                 barcode = input("Barcode: ")
                 quantity = input("Quantity: ")
                 add_item(name, barcode, quantity)
-            elif choice == 2:
+            elif choice == 2: # Update Item
                 input_value = input("Enter name or barcode: ")
-                quantity = input("Quantity: ")
+                quantity = input("Do you want to increment or decrease stock? Use +/-\nQuantity: ")
                 update_item(input_value, quantity)
             elif choice == 3:
                 input_value = input("Enter name or barcode: ")
-                amount = int(input("Amount to remove: "))
-                remove_item(input_value, amount)
-            elif choice == 4:
-                input_value = input("Enter name or barcode: ")
                 find_item(input_value)
+            elif choice == 4:
+                view_items()
             else:
                 break
 
