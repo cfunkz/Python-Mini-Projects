268208908dd46084d205f388eb2e77024a9ea42a
Davids Berzins <116670695+cfunkz@users.noreply.github.com>
2024-01-20 23:18:06 +0000

    Update inventory_management.py

--- inventory_management.py
+++ inventory_management.py
@@ -67,13 +67,12 @@ def find_item(input_value):
 
 
 def view_items():
-    print("Name\t\t| Barcode\t| Quantity")
-    print("="*40)
+    print("\nName\t\t| Barcode\t| Quantity")
+    print("#"*40)
     for name, details in items.items():
         barcode = details["barcode"]
         quantity = details["quantity"]
-        print(f"{name}\t\t| {barcode}\t| {quantity}")
-    print("="*40)
+        print(f"{name}\t\t| {barcode}\t| {quantity}\n")
 
 
 def main():
